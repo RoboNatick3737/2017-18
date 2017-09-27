12e98f9c271c15447cf51be90b2fde393b1f3fa8
Makiah Bennett <makiahtbennett@gmail.com>
2017-09-26 20:53:07 -0400

    Add a shell script which makes connecting to the Moto G phones for wireless debugging a lot easier.

---
+++ wirelessdebughelp.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+echo "First, connect the phone to the USB."
+read -p "Press any key to continue... " -n1 -s
+echo ""
+echo ""
+
+echo "Enter the 'Program & Manage' of the Robot Controller, and connect to the WiFi Direct network it specifies."
+read -p "Press any key to continue... " -n1 -s
+echo ""
+echo ""
+
+echo "Setting up..."
+cd ~/Library/Android/sdk/platform-tools
+./adb kill-server
+./adb devices
+./adb tcpip 5555
+
+echo "Now please remove the USB cable from the phone."
+read -p "Press any key to continue... " -n1 -s
+echo ""
+echo ""
+
+./adb connect 192.168.49.1:5555
+echo "Done! :)"
